2de9c4dc9e76fcb3bfac83f17435b17035391d02
Walker Crouse <walker.crouse@coop.co.uk>
2020-09-22 22:56:34 -0400

    docs
    Signed-off-by: Walker Crouse <walker.crouse@coop.co.uk>

--- zeroconf/src/builder.rs
+++ zeroconf/src/builder.rs
@@ -1,4 +1,8 @@
+//! Provides builder related helpers
+
+/// Implements a `builder()` function for the specified type
 pub trait BuilderDelegate<T: Default> {
+    /// Initializes a new default builder of type `T`
     fn builder() -> T {
         T::default()
     }
