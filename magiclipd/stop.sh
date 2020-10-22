ecf9fccb373134629e9b43edacf5797fd633913c
Walker Crouse <walker.crouse@coop.co.uk>
2020-10-22 10:53:32 -0400

    linux stop script
    Signed-off-by: Walker Crouse <walker.crouse@coop.co.uk>

--- magiclipd/stop.sh
+++ magiclipd/stop.sh
@@ -6,6 +6,11 @@ macos_stop() {
     kill $(pgrep magiclipd)
 }
 
+linux_stop() {
+    systemctl stop magiclipd
+    systemctl status magiclipd
+}
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
     macos_stop
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
